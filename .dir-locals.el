0365a74794deaab4934d517c90ee9dd9b2c8ef0a
aemogie <theaemogie@gmail.com>
2025-12-13 23:34:00 +0530

    peano/church numerals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "rustc main.rs && ./main"))))
+((nil . ((compile-command . "rustc --color=always -C opt-level=1 main.rs && ./main"))))
--- main.rs
+++ main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("hello vexi");
+use std::marker::PhantomData;
+
+pub fn main() {
+    macro_rules! succ {
+        ($n:ty) => {<$n as Nat>::Next};
+    }
+    std::hint::black_box(<
+        succ!(succ!(succ!(succ!(Zero))))
+    >::doo(0, |x| x + 1));
+}
+
+trait Nat: Sized {
+    type Next: Nat;
+    fn doo<T>(i: T, f: fn(T) -> T) -> T;
+}
+
+struct Zero;
+impl Nat for Zero {
+    type Next = Successor<Self>;
+    #[inline(always)]
+    fn doo<T>(i: T, _: fn(T) -> T) -> T {
+        i
+    }
+}
+
+struct Successor<Predecessor> where Predecessor: Nat {
+    __phantom: PhantomData<Predecessor>
+}
+impl<Predecessor: Nat> Nat for Successor<Predecessor> {
+    type Next = Successor<Self>;
+    #[inline(always)]
+    fn doo<T>(i: T, f: fn(T) -> T) -> T {
+        f(Predecessor::doo(i, f))
+    }
 }
